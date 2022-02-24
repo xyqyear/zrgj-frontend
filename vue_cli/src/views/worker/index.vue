4c50915c833aaefddec2c0e5b89e035213cacd7c
waliwaliwa <1990459403@qq.com>
2022-02-24 17:57:25 +0800

    员工管理ｏｋ

--- vue_cli/src/views/worker/index.vue
+++ vue_cli/src/views/worker/index.vue
@@ -1,72 +1,106 @@
 <template>
-  <el-table
-    :data="tableData"
-    style="width: 100%">
-    <el-table-column
-      label="日期"
-      width="180">
-      <template slot-scope="scope">
-        <i class="el-icon-time"></i>
-        <span style="margin-left: 10px">{{ scope.row.date }}</span>
-      </template>
-    </el-table-column>
-    <el-table-column
-      label="姓名"
-      width="180">
+  <el-row class="home" :gutter="20">
+    <div class="operation">
+      <!-- <div>
+        <p style="margin-left:20px;color:#545C64">查找到{{find}}条</p>
+      </div> -->
+      <div style="margin-right:20px;display:flex;">
+        <el-input placeholder="请输入内容" v-model="input" clearable>
+           <el-button slot="append" icon="el-icon-search"></el-button>
+        </el-input>
+      </div>
+         <el-button plain style="margin-left :20px;">添加人员</el-button>
+    </div>
+    <el-col :span="24">
+      <el-card style="margin-top: 20px; height: 460px">
+        <el-table :data="tableData" height:300 style="width: 100%">
+          <el-table-column
+      prop="index"
+      label="序号"
+      sortable="true">
+          </el-table-column>
+          <el-table-column
+      prop="id"
+      label="员工号">
+          </el-table-column>
+          <!-- <el-table-column
+      label="菜品图片"
+      width="100">
       <template slot-scope="scope">
         <el-popover trigger="hover" placement="top">
-          <p>姓名: {{ scope.row.name }}</p>
-          <p>住址: {{ scope.row.address }}</p>
+          <img :src="scope.row.img" style="width:100px;height:100px;" />
           <div slot="reference" class="name-wrapper">
-            <el-tag size="medium">{{ scope.row.name }}</el-tag>
+            <el-tag size="medium" style="color:#3A96FF">查看图片</el-tag>
           </div>
         </el-popover>
       </template>
-    </el-table-column>
-    <el-table-column label="操作">
+          </el-table-column> -->
+          <el-table-column
+      prop="name"
+      label="姓名">
+          </el-table-column>
+          <el-table-column
+      prop="type"
+      label="员工类型">
+          </el-table-column>
+          <el-table-column
+      prop="phone"
+      label="手机号码">
+          </el-table-column>
+          <el-table-column
+      label="操作">
       <template slot-scope="scope">
         <el-button
           size="mini"
-          @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
+          type="text"
+          @click="handleEdit(scope.$index, scope.row)">修改</el-button>
         <el-button
           size="mini"
-          type="danger"
+          type="text"
           @click="handleDelete(scope.$index, scope.row)">删除</el-button>
       </template>
-    </el-table-column>
-  </el-table>
+          </el-table-column>
+        </el-table>
+      </el-card>
+    </el-col>
+  </el-row>
 </template>
-
 <script>
-  export default {
-    data() {
-      return {
-        tableData: [{
-          date: '2016-05-02',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1518 弄'
-        }, {
-          date: '2016-05-04',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1517 弄'
-        }, {
-          date: '2016-05-01',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1519 弄'
-        }, {
-          date: '2016-05-03',
-          name: '王小虎',
-          address: '上海市普陀区金沙江路 1516 弄'
-        }]
-      }
-    },
-    methods: {
-      handleEdit(index, row) {
-        console.log(index, row);
-      },
-      handleDelete(index, row) {
-        console.log(index, row);
-      }
-    }
-  }
+export default {
+  name: "home",
+  data() {
+    return {
+       input: '',
+       find:'22',
+      tableData: [
+        {
+          index:'1',
+          id:'12313',
+          name:'哇哩哇',
+          type:'基层',
+          phone:'1234567881'
+        },
+       {
+          index:'2',
+          id:'12313',
+          name:'哇哩哇',
+          type:'基层',
+          phone:'1234567881'
+        },
+        
+      ],
+    };
+  },
+};
 </script>
+<style lang="less" scoped>
+.operation {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  text-align: center;
+  margin-left: 20px;
+  margin-right: 20px;
+}
+</style>
