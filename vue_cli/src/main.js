1f3662b89b26109dfbef54b19178e56d42b3cb25
waliwaliwa <1990459403@qq.com>
2022-02-26 17:52:07 +0800

    合并服务员端全部菜品

--- vue_cli/src/main.js
+++ vue_cli/src/main.js
@@ -14,7 +14,8 @@ import '../api/mock.js'
 //按需引入
 import { Button, Select,Radio,Container,Aside,Header,Main,
 Menu,Submenu,MenuItem,MenuItemGroup,Dropdown,DropdownMenu,DropdownItem,
-Row,Col,Card,Table,TableColumn,Input,Popover,Tag,Dialog,Collapse,Form,FormItem,Tabs,TabPane,InputNumber,CollapseItem } from 'element-ui';
+Row,Col,Card,Table,TableColumn,Input,Popover,Tag,Dialog,Collapse,Form,
+FormItem,Tabs,TabPane,InputNumber,CollapseItem,Drawer } from 'element-ui';
 
 Vue.config.productionTip = false
 //全引入element的包
@@ -50,6 +51,8 @@ Vue.use(Tabs);
 Vue.use(TabPane);
 Vue.use(InputNumber);
 Vue.use(CollapseItem);
+Vue.use(Drawer);
+
 
 Vue.prototype.$http = http
 
