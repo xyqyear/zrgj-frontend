8b1ec206085880d6e44d2e362b54587684e686ee
MuZi-lh <61682147+MuZi-lh@users.noreply.github.com>
2022-02-28 17:51:40 +0800

    complete the page for waiter to check order

--- vue_cli/api/data.js
+++ vue_cli/api/data.js
@@ -389,11 +389,10 @@ export const upload = (param) => {
 /*
 Body:见文档
 */
-export const getRestaurant = (param) => {
+export const getRestaurant = () => {
   return axios.request({
     url: 'http://www.muzi.fun:5678/api/v1/restaurant/get',
     method:'POST',
-    data:param,
     headers: {
       Authorization: localStorage.getItem('token')
     }
