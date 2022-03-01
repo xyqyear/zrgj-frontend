ff1e708dc5014619bd7e617dd6c27fd9d06f1cb7
xyqyear <215908558@qq.com>
2022-03-01 16:35:44 +0800

    update css for food

--- vue_cli/src/views/serFood/index.vue
+++ vue_cli/src/views/serFood/index.vue
@@ -1,39 +1,65 @@
 <template>
   <div style="position: relative">
     <el-tabs v-model="activeName">
-      <el-tab-pane v-for="(category,index) in dishCategories" :label="category" :name="category">
+      <el-tab-pane
+        v-for="(category, index) in dishCategories"
+        :label="category"
+        :name="category"
+        :key="index"
+      >
         <div>
           <el-row :gutter="10">
-            <el-col :span="4"
+            <el-col
+              :span="4"
               v-for="(item, dishIndex) in dishList"
               :key="item.id"
-              style="{ margin: 0 }"
+              style="
+                 {
+                  margin: 0;
+                }
+              "
             >
-              <el-card v-if="item.category===category || index===0" style="padding: 0; width: 220px; margin-left: 10px">
-                <div>
-                  <img :src="item.imageUrl" height="90px" width="90px"/>
+              <el-card
+                v-if="item.category === category || index === 0"
+                :body-style="{ padding: '0px' }"
+                style="width: 200px; padding: 0px; margin: 20px"
+              >
+                <div
+                  style="position: relative; text-align: center;"
+                >
+                  <img
+                    :src="item.imageUrl"
+                    class="image"
+                    height="190"
+                    width="200"
+                  />
+                  <div style="position: absolute; height: 2px; width: 100%; bottom: 3px; background-color: grey; opacity:0.3"></div>
+                  <div style="position: absolute; height: 32px; width: 100%; bottom: 5px; background-color: black; opacity:0.5;">
+                  </div>
+                  <div style="position: absolute; height: 2px; width: 100%; bottom: 37px; background-color: grey; opacity:0.3"></div>
+                  <div style="position: absolute; height: 2px; width: 100%; bottom: 26px; color: white">{{item.name}}</div>
                 </div>
 
-                <div>
-                  <span>{{ item.name }}</span>
-                </div>
-                <div>
-                  <span>￥{{ item.price }}</span>
-                </div>
-                <div>
-                  <el-input-number
-                    v-model="dishList[dishIndex].amount"
-                    @change="handleChange($event, dishIndex)"
-                    :min="0"
-                    :max="20"
-                  ></el-input-number>
-                </div>
+                <el-row style="margin-top: 10px; margin-bottom: 10px; display: flex; align-items: center;">
+                  <el-col :span="10" style="font-weight: bold; color: #f95a68; ">
+                    {{item.price}}元/份
+                  </el-col>
+                  <el-col :span="14">
+                    <el-input-number
+                      v-model="dishList[dishIndex].amount"
+                      @change="handleChange($event, dishIndex)"
+                      size="mini"
+                      min="0"
+                      max="20"
+                      style="width: 100px"
+                    ></el-input-number>
+                  </el-col>
+                </el-row>
+
               </el-card>
-
             </el-col>
           </el-row>
         </div>
-
       </el-tab-pane>
     </el-tabs>
     <el-button
@@ -41,21 +67,39 @@
       type="primary"
       size="mini"
       style="position: absolute; right: 10px; top: 5px"
-    >创建订单
-    </el-button
+      >创建订单
+    </el-button>
+    <el-drawer
+      title="订单总览"
+      :visible.sync="drawer"
+      :direction="direction"
+      size="40%"
     >
-    <el-drawer title="订单总览" :visible.sync="drawer" :direction="direction" size="40%">
       <template>
         <el-table :data="orderItems" border height="600">
-          <el-table-column prop="name" label="菜名" width="150"></el-table-column>
-          <el-table-column prop="amount" label="数量" width="200"></el-table-column>
-          <el-table-column prop="note" label="备注" width="100"></el-table-column>
+          <el-table-column
+            prop="name"
+            label="菜名"
+            width="150"
+          ></el-table-column>
+          <el-table-column
+            prop="amount"
+            label="数量"
+            width="200"
+          ></el-table-column>
+          <el-table-column
+            prop="note"
+            label="备注"
+            width="100"
+          ></el-table-column>
           <el-table-column prop="price" label="金额"></el-table-column>
         </el-table>
       </template>
       <el-descriptions>
         <el-descriptions-item label="桌号">{{ tableId }}</el-descriptions-item>
-        <el-descriptions-item label="总金额">{{ totalPrice }}</el-descriptions-item>
+        <el-descriptions-item label="总金额">{{
+          totalPrice
+        }}</el-descriptions-item>
         <el-descriptions-item label="下单账号">01</el-descriptions-item>
       </el-descriptions>
       <div class="demo-drawer__footer">
@@ -67,7 +111,7 @@
 </template>
 
 <script>
-import {addOrder, getAllFood} from "../../../api/data";
+import { addOrder, getAllFood } from "../../../api/data";
 
 export default {
   name: "perCen",
@@ -76,18 +120,18 @@ export default {
       activeName: "全部菜品",
       num: 1,
       dishList: [],
-      dishCategories: ['全部菜品', '荤菜', '素菜', '汤类', '小吃', '饮品'],
+      dishCategories: ["全部菜品", "荤菜", "素菜", "汤类", "小吃", "饮品"],
       classifiedDishList: [],
       drawer: false,
       direction: "rtl",
       orderItems: [],
       totalPrice: 0,
-      tableId: 5
+      tableId: 5,
     };
   },
 
   mounted() {
-    this.refreshDishList()
+    this.refreshDishList();
 
     /*    菜品分类
         this.classifiedDishList.push(this.dishList)
@@ -99,38 +143,35 @@ export default {
     createNewOrder() {
       this.drawer = true;
       this.orderItems = [];
-      this.totalPrice = 0.0
+      this.totalPrice = 0.0;
       for (const dish of this.$data.dishList) {
         if (dish.amount != 0) {
           this.orderItems.push(dish);
           this.totalPrice += dish.amount * dish.price;
         }
       }
-
     },
     uploadOrder() {
       let newOrder = {
-        "tableId": this.tableId,
-        "orderItems": this.orderItems
-      }
+        tableId: this.tableId,
+        orderItems: this.orderItems,
+      };
       addOrder(newOrder)
-        .then(res => {
+        .then((res) => {
           this.$message({
-            message: '创建订单成功',
-            type: 'success'
+            message: "创建订单成功",
+            type: "success",
           });
-          this.refreshDishList()
+          this.refreshDishList();
           this.drawer = false;
         })
-        .catch(error => {
+        .catch((error) => {
           console.log(error);
-          this.$message.error('网络异常，生成订单失败');
-        })
-
+          this.$message.error("网络异常，生成订单失败");
+        });
     },
     refreshDishList() {
-      getAllFood()
-      .then(res =>{
+      getAllFood().then((res) => {
         this.dishList = res.data.data;
         // 给每个菜品加上orderItem所需属性
         for (let i = 0; i < this.dishList.length; i++) {
@@ -138,14 +179,14 @@ export default {
           this.dishList[i].note = "";
           this.dishList[i].dishId = this.dishList[i].id;
         }
-      })
+      });
     },
     handleChange(dishIndex) {
       // this.dishList[dishIndex].amount += 1;
       this.$forceUpdate();
       console.log(dishIndex);
     },
-/*    getDishOfCategory(targetCategory) {
+    /*    getDishOfCategory(targetCategory) {
       let targetDishList = [];
       for (const dish of this.$data.dishList) {
         if (dish.category === targetCategory) {
@@ -159,8 +200,7 @@ export default {
         .then((_) => {
           done();
         })
-        .catch((_) => {
-        });
+        .catch((_) => {});
     },
   },
 };
