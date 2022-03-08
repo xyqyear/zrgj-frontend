5b9b440487be7a96f0d86b10e8d0e550c0088aae
xyqyear <215908558@qq.com>
2022-03-08 16:00:58 +0800

    add linting to project

--- vue_cli/.eslintrc.js
+++ vue_cli/.eslintrc.js
@@ -10,8 +10,7 @@ module.exports = {
   parserOptions: {
     parser: 'babel-eslint'
   },
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
-  }
+  "rules": {
+    "prettier/prettier": ["error"]
+  },
 }
