051ae22a39f55fc362b5f2fbaaaeb96a252dc0b2
xyqyear <215908558@qq.com>
2022-03-10 10:26:35 +0800

    same as the previous commit

--- vue_cli/src/store/orders.js
+++ vue_cli/src/store/orders.js
@@ -8,7 +8,7 @@ export default {
   // 显示的更改state
   mutations: {
     refreshOrderList(state, orderList) {
-      this.state.orderList = orderList
+      state.orderList = orderList
     }
   },
   // 过滤state中的数据
@@ -20,7 +20,6 @@ export default {
     // 获取通知列表
     async getOrderListFromServer({ dispatch, commit }) {
       const orderList = (await getCurrOrders()).data.data
-      console.log('shit:', orderList)
       commit('refreshOrderList', orderList)
     }
   }
